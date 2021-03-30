cc321c4a6ba8049bc4d9d4cb5e7b87977d9c83ae
BWsix <itisbw62@gmail.com>
2021-03-31 00:14:01 +0800

    init

---
+++ index/views.py
@@ -0,0 +1,36 @@
+import json, base64, io
+
+from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+
+from PIL import Image
+import numpy as np
+
+class HomeView(View):
+  def get(self, req):
+    return render(req, 'home.html')
+
+class ApiView(View):
+  def post(self, req):
+    body_unicode = req.body.decode('utf-8')
+    body = json.loads(body_unicode)
+
+    img_64 = body['dataUrl'].split('base64,')[1]
+    img_raw = base64.b64decode(img_64)
+
+    stream = io.BytesIO(img_raw)
+
+    img = Image.open(stream)
+    img = img.resize((28, 28))
+    img.save('tester.png')
+
+    img_np = np.array(img)
+
+    # result = img_to_number(img_np)
+
+    data = {
+      'reslt': 'some output number'
+    }
+
+    return JsonResponse(data)
